960bbc6fefa2e623737c500bfb96d3d9feef664b
Tiago Cappellaro <tiagocap@gmail.com>
2024-10-09 12:21:02 -0300

    Update LoginController.php

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,1 +1,15 @@
+<?php
+require_once __DIR__ . '/../models/Database.php';
 
+class LoginController {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    public function autenticar($usuario, $senha) {
+        return $this->db->verificarUsuario($usuario, $senha);
+    }
+}
+?>
